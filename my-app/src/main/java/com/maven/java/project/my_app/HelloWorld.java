b6f1deb5be661e41f6ce4688f3cec09efedbb06c
zhanglei007lei <1250253900@qq.com>
2017-05-27 11:44:36 +0800

    update

--- my-app/src/main/java/com/maven/java/project/my_app/HelloWorld.java
+++ my-app/src/main/java/com/maven/java/project/my_app/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	 public String sayHello()
 	    {
-	        return "Hello Maven";
+	        return "updat this Hello Maven";
 	    }
 
 	    public static void main(String[] args)
